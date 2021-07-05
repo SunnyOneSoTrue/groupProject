4662f50e354df064c94f0af6b5aefcf3b94de5f2
SunnyOneSoTrue <kakhianisandro12@gmail.com>
2021-07-05 10:13:34 +0400

    Initial Commit

---
+++ GropProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GropProject
+//
+//  Created by USER on 05.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
