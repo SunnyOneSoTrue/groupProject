4b7451e62d3f369573c95f95cde7dac5cea9b16a
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-05 01:44:17 -0700

    added gradient to the image

--- GropProject/Cell/welcomeCell.swift
+++ GropProject/Cell/welcomeCell.swift
@@ -25,5 +25,25 @@ class welcomeCell: UICollectionViewCell {
         imageView!.frame = bounds
         imageView!.image = image
         addSubview(imageView!)
+        setUpImageGradient()
+    }
+    
+    func setUpImageGradient() {
+        guard let imageView = imageView else { return }
+        let view  = UIView(frame: bounds)
+        let gradient = CAGradientLayer()
+        gradient.frame = view.frame
+
+        let color1 = UIColor.black.withAlphaComponent(0).cgColor
+        let color2 = UIColor.black.withAlphaComponent(1).cgColor
+    
+        gradient.colors = [color1, color2]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.4)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        view.layer.insertSublayer(gradient, at: 0)
+
+        imageView.addSubview(view)
+        imageView.bringSubviewToFront(view)
     }
 }
